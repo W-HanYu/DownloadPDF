5963b47ff56ae516a3d2fa6b16e96b5432e8f946
wangtao <1453300745@qq.com>
2024-09-01 12:42:59 +0800

    feat: JSPDF + html2canvas A4分页截断

---
+++ pdf-demo/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PdfDownload from "./components";
+
+function App() {
+  return (
+    <>
+      <PdfDownload />
+    </>
+  );
+}
+
+export default App;
---
+++ pdf-demo/src/components/constants.ts
@@ -0,0 +1,9 @@
+export const PDF_PRINT_IDS = {
+  header: "print-header",
+  footer: "print-footer",
+  content1: "print-content",
+  content2: "print-content2",
+  content3: "print-content3",
+  content4: "print-content4",
+  content5: "print-content5",
+};
